ae179ea9d69abc10582ac07530fcc676fca1a437
ranaglmsr132 <azrarana.gulumser@std.yeditepe.edu.tr>
2024-12-25 16:55:53 +0300

    python2

--- main.py
+++ main.py
@@ -1,13 +1,48 @@
-note=float(input("notunuz"))
-deneyim=int(input("deneyiminiz"))
-if note>90:
-  print("alındı")
-elif deneyim==5 and note==70:
-  print("alındınız")
-if note>70:
-  print("mulakat")
-elif deneyim>4:
-  print("mulakat")
+note=float(input("notunuz nedir?"))
+if note<80:
+  print("ablehnen")
 else:
-  print("reddedildinmiz")
+  print("başarılı")
+age=int(input("yasınız nedir?"))
+if 20<age<50:
+  print("einstellen")
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
