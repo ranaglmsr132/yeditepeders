e8c6347bdf7074e9c0b05b5403e4498cb1aa9498
ranaglmsr132 <azrarana.gulumser@std.yeditepe.edu.tr>
2025-04-21 16:27:15 +0300

    Add files via upload

---
+++ 2.soru.py
@@ -0,0 +1,20 @@
+ad=input("Adınızı Giriniz: ")
+soyad=input("Soyadınızı Giriniz: ")
+print(str("Ad: ")+ ad)
+print(str("Soyad: ")+ soyad)
+print("Çıktı: "+ ad[0]+"."+soyad[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
