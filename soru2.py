bb80f747ac96cc7e2668f68f838d7dff2aeba18b
ranaglmsr132 <azrarana.gulumser@std.yeditepe.edu.tr>
2025-04-28 17:37:16 +0300

    Add files via upload

---
+++ soru2.py
@@ -0,0 +1,9 @@
+mesafe1=input("1.x koordinat girin")
+mesafe2=input("1.y koordinat girin")
+
+mesafe3=input("2.x koordinat girin")
+mesafe4=input("2.y koordinat girin")
+
+xfark = mesafe3[0] - mesafe1[0]
+yfark = mesafe4[1] - mesafe2[1]
+
