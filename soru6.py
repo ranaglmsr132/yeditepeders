2e69736dbb624ef217595e58c175bf8f498c7514
ranaglmsr132 <azrarana.gulumser@std.yeditepe.edu.tr>
2025-04-28 18:03:11 +0300

    Add files via upload

---
+++ soru6.py
@@ -0,0 +1,14 @@
+def toplam(listeler):
+    sayac = 0
+    for sayi in listeler:
+        sayac += 1
+    
+listeler=input("Listenizi griniz: ")
+eleman_sayisi = toplam(listeler)
+print(eleman_sayisi)
+
+
+
+
+
+
