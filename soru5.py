d240f543ba16783c702356918c8cee132f858ed6
ranaglmsr132 <azrarana.gulumser@std.yeditepe.edu.tr>
2025-04-28 17:56:16 +0300

    Add files via upload

---
+++ soru5.py
@@ -0,0 +1,16 @@
+sayi1=input("1.Sayıyı Giriniz: ")
+sayi2=input("2.Sayıyı Giriniz: ")
+sayi3=input("3.Sayıyı Giriniz: ")
+
+if (sayi1>=sayi2) and (sayi1>=sayi3):
+   enBuyuk = sayi1
+elif (sayi2 >= sayi1) and (sayi2 >= sayi3):
+   enBuyuk = sayi2
+else:
+   enBuyuk = sayi3
+print(enBuyuk)
+
+
+
+
+
